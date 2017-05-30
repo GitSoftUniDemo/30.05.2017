f73c495f3fc669c79382ddd9825fd962ebeb3884
Mariana Kovacheva <kovachevamm@gmail.com>
2017-05-30 19:37:30 +0300

    Added new comments to the program

--- TimeCalculator.java
+++ TimeCalculator.java
@@ -17,11 +17,13 @@ public class TimeCalculator {
     public static void main(String[] args) {
         double seconds = 1540800; 
         double minutes = seconds / 60; 
-        double hours = minutes/60;
+        double hours = minutes/60;//divided the received minites from the upper row
         double days = hours/24; 
         double weeks = days/7;
         double years = days/365;
         
+		//We need to print the results receved in the variables
+
         System.out.println("Minutes:" + minutes);
         System.out.println("Hours:" + hours);
         System.out.println("Days:" + days);
